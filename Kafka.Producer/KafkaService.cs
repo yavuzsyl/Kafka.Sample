4399ba401a4df26ad91266da7e3eb2aee06d227c
yavuzsyl <yavuzsyl@gmail.com>
2025-06-02 00:54:04 +0300

    intro "creating a topic"

---
+++ Kafka.Producer/KafkaService.cs
@@ -0,0 +1,27 @@
+﻿using Confluent.Kafka.Admin;
+using Confluent.Kafka;
+
+namespace Kafka.Producer;
+
+public static class KafkaService
+{
+    static string TopicName = "topic_one";
+
+    public static async Task CreateTopicAsync()
+    {
+        using var adminClient = new AdminClientBuilder(new AdminClientConfig()
+        { BootstrapServers = "localhost:9094" }).Build();
+
+        try
+        {
+            await adminClient.CreateTopicsAsync(
+            [
+                new TopicSpecification(){ Name = TopicName, NumPartitions = 3, ReplicationFactor = 1 }
+            ]);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+    }
+}
--- Kafka.Producer/Program.cs
+++ Kafka.Producer/Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using Kafka.Producer;
+
 Console.WriteLine("Producer");
+
+await KafkaService.CreateTopicAsync();
