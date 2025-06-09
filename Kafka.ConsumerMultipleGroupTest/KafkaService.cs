c8c4be2f9b0e9e6ca9d4852bf2a9d8e36be0b071
yavuzsyl <yavuzsyl@gmail.com>
2025-06-09 15:06:24 +0300

    consumer group 2

---
+++ Kafka.ConsumerMultipleGroupTest/KafkaService.cs
@@ -0,0 +1,26 @@
+﻿using Confluent.Kafka;
+
+namespace Kafka.ConsumerMultipleGroupTest;
+internal class KafkaService
+{
+    public async Task ConsumeMessageWithNullKeyAsync(string topicName)
+    {
+        var config = new ConsumerConfig()
+        {
+            BootstrapServers = "localhost:9094",
+            GroupId = "use-case-1-group-2",
+            AutoOffsetReset = AutoOffsetReset.Earliest
+        };
+
+        var consumer = new ConsumerBuilder<Null, string>(config).Build();
+        consumer.Subscribe(topicName);
+
+        while (true)
+        {
+            var consumeResult = consumer.Consume(5000);
+            if (consumeResult != null)
+                Console.WriteLine($"consumed message: {consumeResult.Message.Value}");
+            await Task.Delay(500);
+        }
+    }
+}
---
+++ Kafka.ConsumerMultipleGroupTest/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Kafka.ConsumerMultipleGroupTest;
+
+Console.WriteLine("Consumer 2");
+
+var kafkaService = new KafkaService();
+var topicName = "use-case-1-topic";
+await kafkaService.ConsumeMessageWithNullKeyAsync(topicName);
+Console.ReadLine();
